6d7256455e3ee6e0a6884a8bbae2f3bd6b01ae31
Denis <denisrubel40@gmail.com>
2023-09-28 17:45:21 +0300

    Updated the registration method

--- SocialSync/Controllers/AccountController.cs
+++ SocialSync/Controllers/AccountController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using SocialSync.Data;
 using SocialSync.DTOs;
@@ -13,11 +14,13 @@ namespace SocialSync.Controllers
     {
         private readonly DataContext _context;
         private readonly ITokenService _tokenService;
+        private readonly IMapper _mapper;
 
-        public AccountController(DataContext context, ITokenService tokenService)
+        public AccountController(DataContext context, ITokenService tokenService, IMapper mapper)
         {
             _context = context;
             _tokenService = tokenService;
+            _mapper = mapper;
         }
 
         [HttpPost("register")]
@@ -26,14 +29,13 @@ namespace SocialSync.Controllers
             if (await UserExists(registerDto.Username))
                 return BadRequest("Username is taken");
 
+            var user = _mapper.Map<AppUser>(registerDto);
+
             using var hmac = new HMACSHA512();
 
-            var user = new AppUser
-            {
-                UserName = registerDto.Username.ToLower(),
-                PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password)),
-                PasswordSalt = hmac.Key
-            };
+            user.UserName = registerDto.Username.ToLower();
+            user.PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password));
+            user.PasswordSalt = hmac.Key;
 
             _context.Users.Add(user);
             await _context.SaveChangesAsync();
@@ -41,7 +43,8 @@ namespace SocialSync.Controllers
             return new UserDto
             {
                 Username = user.UserName,
-                Token = _tokenService.CreateToken(user)
+                Token = _tokenService.CreateToken(user),
+                KnownAs = user.KnownAs
             };
         }
 
@@ -69,7 +72,8 @@ namespace SocialSync.Controllers
             {
                 Username = user.UserName,
                 Token = _tokenService.CreateToken(user),
-                PhotoUrl = user.Photos.FirstOrDefault(x => x.IsMain)?.Url
+                PhotoUrl = user.Photos.FirstOrDefault(x => x.IsMain)?.Url,
+                KnownAs = user.KnownAs
             };
         }
 
--- SocialSync/DTOs/RegisterDto.cs
+++ SocialSync/DTOs/RegisterDto.cs
@@ -6,8 +6,24 @@ namespace SocialSync.DTOs
     {
         [Required]
         public string Username { get; set; }
+        
+        [Required] 
+        public string KnownAs { get; set; }
+
+        [Required] 
+        public string Gender { get; set; }
+
+        [Required] 
+        public DateOnly? DateOfBirth { get; set; } 
+
+        [Required] 
+        public string City { get; set; }
+
+        [Required] 
+        public string Country { get; set; }
 
         [Required]
+        [StringLength(8, MinimumLength = 4)]
         public string Password { get; set; }
     }
 }
