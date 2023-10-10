7eaf6e0977074828ebc4b5d02f2e60a26f8558e6
Denis <denisrubel40@gmail.com>
2023-10-10 23:15:09 +0300

    Reworked the messaging feature using SignalR

--- SocialSync/Repository/MessageRepository.cs
+++ SocialSync/Repository/MessageRepository.cs
@@ -19,20 +19,19 @@ namespace SocialSync.Repository
             _context = context;
             _mapper = mapper;
         }
-
         public void AddMessage(Message message)
         {
             _context.Messages.Add(message);
         }
 
-        public void DeleteMessage(Message message)
-        {
-            _context.Messages.Remove(message);
-        }
-
         public async Task<Message> GetMessage(int id)
         {
             return await _context.Messages.FindAsync(id);
+        }
+
+        public void DeleteMessage(Message message)
+        {
+            _context.Messages.Remove(message);
         }
 
         public async Task<PagedList<MessageDto>> GetMessagesForUser(MessageParams messageParams)
@@ -86,9 +85,33 @@ namespace SocialSync.Repository
             return _mapper.Map<IEnumerable<MessageDto>>(messages);
         }
 
+
         public async Task<bool> SaveAllAsync()
         {
             return await _context.SaveChangesAsync() > 0;
         }
+
+        public void AddGroup(Group group)
+        {
+            _context.Groups.Add(group);
+        }
+
+        public void RemoveConnection(Connection connection)
+        {
+            _context.Connections.Remove(connection);
+        }
+
+        public async Task<Connection> GetConnection(string connectionId)
+        {
+            return await _context.Connections.FindAsync(connectionId);
+        }
+
+        public async Task<Group> GetMessageGroup(string groupName)
+        {
+            return await _context.Groups
+                .Include(x => x.Connections)
+                .FirstOrDefaultAsync(x => x.Name == groupName);
+        }
+
     }
 }
---
+++ SocialSync/SignalR/MessageHub.cs
@@ -0,0 +1,118 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.SignalR;
+using SocialSync.DTOs;
+using SocialSync.Entities;
+using SocialSync.Extensions;
+using SocialSync.Repository.IRepository;
+
+namespace SocialSync.SignalR
+{
+    [Authorize]
+    public class MessageHub : Hub
+    {
+        private readonly IMessageRepository _messageRepository;
+        private readonly IUserRepository _userRepository;
+        private readonly IMapper _mapper;
+        private readonly IHubContext<PresenceHub> _presenceHub;
+
+        public MessageHub(IMessageRepository messageRepository, IUserRepository userRepository,
+            IMapper mapper, IHubContext<PresenceHub> presenceHub)
+        {
+            _mapper = mapper;
+            _presenceHub = presenceHub;
+            _messageRepository = messageRepository;
+            _userRepository = userRepository;
+        }
+
+        public override async Task OnConnectedAsync()
+        {
+            var httpContext = Context.GetHttpContext();
+            var otherUser = httpContext.Request.Query["user"];
+            var groupName = GetGroupName(Context.User.GetUsername(), otherUser);
+            await AddToGroup(groupName);
+
+            await Groups.AddToGroupAsync(Context.ConnectionId, groupName);
+
+            var messages = await _messageRepository.GetMessageThread(Context.User.GetUsername(), otherUser);
+
+            await Clients.Group(groupName).SendAsync("ReceiveMessageThread", messages);
+        }
+
+        public override async Task OnDisconnectedAsync(Exception ex)
+        {
+            await RemoveFromMessageGroup();
+            await base.OnDisconnectedAsync(ex);
+        }
+
+        public async Task SendMessage(CreateMessageDto createMessageDto)
+        {
+            var username = Context.User.GetUsername();
+
+            if (username == createMessageDto.RecipientUsername.ToLower())
+                throw new HubException("You cannot send messages to yourself");
+
+            var sender = await _userRepository.GetUserByUsernameAsync(username);
+            var recipient = await _userRepository.GetUserByUsernameAsync(createMessageDto.RecipientUsername);
+
+            if (recipient == null) 
+                throw new HubException("Not found user");
+
+            var message = new Message
+            {
+                Sender = sender,
+                Recipient = recipient,
+                SenderUsername = sender.UserName,
+                RecipientUsername = recipient.UserName,
+                Content = createMessageDto.Content
+            };
+
+            var groupName = GetGroupName(sender.UserName, recipient.UserName);
+
+            var group = await _messageRepository.GetMessageGroup(groupName);
+
+            if (group.Connections.Any(x => x.Username == recipient.UserName))
+            {
+                message.DateRead = DateTime.UtcNow;
+            }
+
+            _messageRepository.AddMessage(message);
+
+            if (await _messageRepository.SaveAllAsync())
+            {
+                await Clients.Group(groupName).SendAsync("NewMessage", _mapper.Map<MessageDto>(message));
+            }
+        }
+
+        private string GetGroupName(string caller, string other)
+        {
+            var stringCompare = string.CompareOrdinal(caller, other) < 0;
+            return stringCompare ? $"{caller}-{other}" : $"{other}-{caller}";
+        }
+
+        private async Task<bool> AddToGroup(string groupName)
+        {
+            var group = await _messageRepository.GetMessageGroup(groupName);
+            var connection = new Connection(Context.ConnectionId, Context.User.GetUsername());
+
+            if (group == null)
+            {
+                group = new Group(groupName);
+                _messageRepository.AddGroup(group);
+            }
+
+            group.Connections.Add(connection);
+
+            return await _messageRepository.SaveAllAsync();
+        }
+
+        private async Task RemoveFromMessageGroup()
+        {
+            var connection = await _messageRepository.GetConnection(Context.ConnectionId);
+
+            _messageRepository.RemoveConnection(connection);
+
+            await _messageRepository.SaveAllAsync();
+        }
+    }
+}
