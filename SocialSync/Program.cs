9f469abed4a65f074e443d353d11c051e96a2ca1
Denis <denisrubel40@gmail.com>
2023-12-08 22:30:32 +0200

    Preparing the API for publication

--- SocialSync/Program.cs
+++ SocialSync/Program.cs
@@ -39,9 +39,13 @@ app.UseHttpsRedirection();
 app.UseAuthentication();
 app.UseAuthorization();
 
+app.UseDefaultFiles();
+app.UseStaticFiles();
+
 app.MapControllers();
 app.MapHub<PresenceHub>("hubs/presence");
 app.MapHub<MessageHub>("hubs/message");
+app.MapFallbackToController("Index", "Fallback");
 
 using var scope = app.Services.CreateScope();
 var services = scope.ServiceProvider;
